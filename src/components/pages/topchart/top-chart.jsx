4a8659fadf4b7dba1ef38e74f68b5a8e070bc3d4
sara-likaount <saralikaunt@gmail.com>
2022-10-22 02:00:34 +0300

    n

--- src/components/pages/topchart/top-chart.jsx
+++ src/components/pages/topchart/top-chart.jsx
@@ -2,44 +2,28 @@ import "./topchart.css";
 import React from "react";
 import Table from 'react-bootstrap/Table';
 import getData from "../../../service/Apidatacall"
+// import { useState } from "react";
 
 function Topchart() {
-  // const [api, setApi] = useState([])
-  getData(). then(
+  // const [api, setApi] = useState()
 
-  )
+
+  
+  
   return (
-    <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
-  );
-};
+    
+      getData()
+      .then((api)=>{
+      <Table striped bordered hover variant="dark">
+        <thead><tr>Id</tr><tr>Channal Title</tr><tr>Channal Description</tr></thead>
+        <tbody>
+          <tr><td>{api.id}</td><td>{api.title}</td><td>{api.text}</td></tr>
+          </tbody>
+      </Table>}
+      ))
 
+
+
+      }        
 export default Topchart;
 
