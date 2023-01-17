8cdff4fb2896ff854b95f50a83e9f63e760f598d
sara-likaount <saralikaunt@gmail.com>
2023-01-17 16:17:01 +0200

    user loading

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import Routing from './router/routing/routing';
 import { auth, UserContext} from './context/usercontext';
 import { HomeNav } from './components/featurs/homeNav/HomeNav';
 import {useAuthState} from "react-firebase-hooks/auth";
+import Imges from './components/featurs/imges/imges';
 
 
 
 function App() {
 
   const [user, loading]= useAuthState(auth);
+ 
 
   return (
 
     <UserContext.Provider value={{user}}>
       <Router>
-        <HomeNav/>
+        {loading ? <Imges className={"imges"} url={"loading.gif"} height={20} width={100}/>: <HomeNav/>}
         <Routing/> 
         <Footer/>
       </Router>
