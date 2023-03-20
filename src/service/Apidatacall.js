212765bbbbd028a26649434118de8b8a1cb71c58
sara-likaount <saralikaunt@gmail.com>
2023-03-21 00:20:55 +0200

    api addras

--- src/service/Apidatacall.js
+++ src/service/Apidatacall.js
@@ -15,9 +15,10 @@ function Apidatacall(){
     };
 
     const getData = async () => {
-        const response = await fetch("http://localhost:3001/Api")
+        const response = await fetch("http://localhost:5000/Channels")
         const json = await response.json()
         setApi(json)
+        
     };
 
     useEffect(() => {
